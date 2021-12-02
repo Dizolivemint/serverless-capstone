d570a94b422d1e1a7a3ef6035869d4c84375936a
Miles Exner <dizolivemint@gmail.com>
2021-12-02 16:40:06 -0700

    Use imported getuserid

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,16 +4,11 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { createTodoItem } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
 
 export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
-  // TODO: Implement creating a new TODO item
-
-  const authorization = event.headers.Authorization
-  const split = authorization.split(' ')
-  const jwtToken = split[1]
-
-  const newItem = await createTodoItem(newTodo, jwtToken)
+  const newItem = await createTodoItem(newTodo, getUserId(event))
 
   return {
     statusCode: 201,
