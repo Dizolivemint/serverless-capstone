9209a079931582caea69ec4cba736d8d75ae1bc6
Miles Exner <dizolivemint@gmail.com>
2021-12-08 15:25:16 -0700

    Fix error Object is of type 'unknown'

--- client/src/components/EditTodo.tsx
+++ client/src/components/EditTodo.tsx
@@ -58,7 +58,11 @@ export class EditTodo extends React.PureComponent<
 
       alert('File was uploaded!')
     } catch (e) {
-      alert('Could not upload a file: ' + e.message)
+      let errorMessage = "Could not upload a file"
+      if (e instanceof Error) {
+        errorMessage = `${errorMessage}: ${e.message}`
+      }
+      alert(errorMessage)
     } finally {
       this.setUploadState(UploadState.NoUpload)
     }
