7e62026b43f3fb2163bf4b6512097350b585a730
Miles Exner <dizolivemint@gmail.com>
2021-12-08 15:21:10 -0700

    Fix bucketname and expiration type

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -5,8 +5,8 @@ const XAWS = AWSXRay.captureAWS(AWS)
 
 export class AttachmentUtils {
   constructor(
-    private readonly bucketName = process.env.IMAGES_S3_BUCKET,
-    private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION,
+    private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
+    private readonly urlExpiration: number = +process.env.SIGNED_URL_EXPIRATION,
     private readonly s3 = new XAWS.S3({ signatureVersion: 'v4'})) {
   }
   async createAttachmentPresignedUrl(todoId: string) {
@@ -16,8 +16,6 @@ export class AttachmentUtils {
       Expires: this.urlExpiration
     })
 
-    return {
-      uploadUrl
-    }
+    return uploadUrl
   }
 }
