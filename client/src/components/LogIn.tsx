38d2f665ce866eb33b0fdec27879d22b99d8fa40
Miles Exner <dizolivemint@gmail.com>
2021-12-23 17:58:57 -0700

    Add branding info

--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Auth from '../auth/Auth'
-import { Button } from 'semantic-ui-react'
+import { Button, Header } from 'semantic-ui-react'
 
 interface LogInProps {
   auth: Auth
@@ -16,9 +16,20 @@ export class LogIn extends React.PureComponent<LogInProps, LogInState> {
   render() {
     return (
       <div>
+        <Header as="h1">New Year Goals</Header>
+        <h5 style={
+          {margin: "-1rem 124px 1rem"}
+        }>by&nbsp;Milesoft</h5>
         <Button onClick={this.onLogin} size="huge" color="olive">
           Log in
         </Button>
+        <ul>
+          <li>Track your goals</li>
+          <li>Add images to your goals</li>
+          <li>Share and view public goals</li>
+          <li>Free and private*</li>
+        </ul>
+        <p>*Your information will not be sold or used for advertising</p>
       </div>
     )
   }
